3cbab43165d1ee6583085806bd671f0308d63ca7
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-08-24 18:29:25 +0200

    Initial commit

---
+++ src/main/java/pl/zdz/securiti/Quotation.java
@@ -0,0 +1,31 @@
+package pl.zdz.securiti;
+
+public class Quotation {
+
+    private String author;
+    private String content;
+
+    public Quotation() {
+    }
+
+    public Quotation(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
